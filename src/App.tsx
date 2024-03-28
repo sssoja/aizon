9041936b6cbd54d2b4688c2ac29796801ffa0423
Sophie <sophie@sssoja.com>
2024-03-28 23:35:21 +0000

    refactor: replace css styles with utility classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Tab from "./Tabs";
+import Tabs from "./Tabs";
 
 const App = () => {
   return (
-    <Tab
+    <Tabs
       tabs={[
-        { title: "Tab 1", content: <div>Tab 1 Content</div> },
-        { title: "Tab 2", content: <div>Tab 2 Content</div> },
+        { title: "tab 1", content: <div>Content of tab 1</div> },
+        { title: "tab 2", content: <div>Content of tab 2</div> },
       ]}
     />
   );
--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -5,25 +5,39 @@ type Tab = {
   content: any;
 };
 
-const Tab = ({ tabs }: { tabs: Tab[] }) => {
+const Tabs = ({ tabs }: { tabs: Tab[] }) => {
   const [activeTab, setActiveTab] = useState(0);
 
   return (
-    <div className="tab-container">
-      <div className="tab-headers">
+    <div className="flex flex-col w-full">
+      <div className="display: flex">
         {tabs.map((tab: Tab, index: number) => (
           <button
             key={index}
-            className={index === activeTab ? "active" : ""}
+            className={
+              index === activeTab
+                ? "flex-[auto] cursor-pointer p-2 border-b-2 border-b-customBlue border-solid"
+                : "flex-[auto] cursor-pointer p-2 border-b-2 border-b-customGray border-solid"
+            }
             onClick={() => setActiveTab(index)}
           >
-            {tab.title}
+            <span
+              className={
+                index === activeTab
+                  ? "text-customBlue text-sm font-semibold text-center"
+                  : "text-customGray text-sm font-semibold text-center"
+              }
+            >
+              {tab.title}
+            </span>
           </button>
         ))}
       </div>
-      <div className="tab-content">{tabs[activeTab].content}</div>
+      <div className="text-sm text-customGray px-2 py-4">
+        {tabs[activeTab].content}
+      </div>
     </div>
   );
 };
 
-export default Tab;
+export default Tabs;
